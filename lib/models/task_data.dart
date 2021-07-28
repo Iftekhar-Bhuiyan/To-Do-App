77b6baf0b42f4563a9006bb6e225335387312cd5
Iftekhar <iftekhar.bh.emon@gmail.com>
2021-07-28 18:16:08 +0600

    provider

---
+++ lib/models/task_data.dart
@@ -0,0 +1,12 @@
+import 'package:flutter/cupertino.dart';
+import 'package:flutter/material.dart';
+import 'package:flutter/foundation.dart';
+import 'package:to_do_app/models/task.dart';
+
+class TaskData extends ChangeNotifier {
+  List<Task> tasks = [
+    Task(name: 'Buy Milk', isDone: false),
+    Task(name: 'Buy Eggs', isDone: false),
+    Task(name: 'Buy Bread', isDone: false),
+  ];
+}
